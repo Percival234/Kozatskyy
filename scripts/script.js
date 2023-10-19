2c4f571035dab616a8a286d2ad075196aa461c43
Percival234 <abaduyaprise@gmail.com>
2023-10-19 22:41:41 +0300

    image clicker

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,16 @@
 const button = document.querySelector('.change-button');
-const wrapper = document.body;
-const bg = document.querySelector('.bg-image');
+const wrapper = document.querySelector('.wrapper');
+const imageButton = document.querySelectorAll('.menu__point-button');
+const images = document.querySelectorAll('.menu__point-image-container');
 
 button.addEventListener('click', () => {
   wrapper.classList.toggle('active');
 });
 
-const images = document.querySelectorAll('.menu__point-image-container');
-
-images.forEach((image) => {
-  image.addEventListener('click', () => {
-    image.style.display = 'none';
+imageButton.forEach((btn, index) => {
+  btn.addEventListener('click', () => {
+    btn.textContent = btn.textContent === 'Відкрити фото' ? 'Закрити фото' : 'Відкрити фото';
+    btn.classList.toggle('active');
+    images[index].classList.toggle('active');
   });
 });
