810f3ab4b609280113c86b6075fbadc9dd155c23
Percival234 <abaduyaprise@gmail.com>
2023-10-21 15:26:21 +0300

    3 theme

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,5 @@
-const button = document.querySelector('.change-button');
-const wrapper = document.querySelector('.wrapper');
 const imageButton = document.querySelectorAll('.menu__point-button');
 const images = document.querySelectorAll('.menu__point-image-container');
-
-button.addEventListener('click', () => {
-  wrapper.classList.toggle('active');
-});
 
 imageButton.forEach((btn, index) => {
   btn.addEventListener('click', () => {
